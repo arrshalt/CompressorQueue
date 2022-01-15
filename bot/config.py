f6e47601206ff5fdf8af43c16fbc594197ae9a40
arrshalt <97388525+arrshalt@users.noreply.github.com>
2022-01-15 09:35:00 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -21,8 +21,11 @@ try:
     BOT_TOKEN = "5099097229:AAFC4WshHIqZcNceccJeNORoC92JX7jLOII"
     DEV = 1222651878
     OWNER = "1222651878 1562355479"
-    FFMPEG = ffmpeg -i "{}" -preset fast -c:v libx265 -crf 24 -s 1280x720 -tune psnr -b:v 2M -metadata title="Animax"  -metadata:s:v title="Animax Industry - 720p - HEVC"  -metadata:s:a title="Animax" -metadata:s:s title="Animax Subs" -map 0:v -c:a aac -b:a 256k -map 0:a -c:s copy -map 0:s? -max_muxing_queue_size 999999 "{}" -y
-    THUMB = https://telegra.ph/file/bc7f04c92fe8f2c0abcfe.jpg
+    FFMPEG = config(
+        "FFMPEG",
+        default='ffmpeg -i "{}" -preset fast -c:v libx265 -crf 24 -s 1280x720 -tune psnr -b:v 2M -metadata title="Animax"  -metadata:s:v title="Animax Industry - 720p - HEVC"  -metadata:s:a title="Animax" -metadata:s:s title="Animax Subs" -map 0:v -c:a aac -b:a 256k -map 0:a -c:s copy -map 0:s? -max_muxing_queue_size 999999 "{}" -y'
+    )
+        THUMB = https://telegra.ph/file/bc7f04c92fe8f2c0abcfe.jpg
 except Exception as e:
     print("Environment vars Missing")
     print("something went wrong")
